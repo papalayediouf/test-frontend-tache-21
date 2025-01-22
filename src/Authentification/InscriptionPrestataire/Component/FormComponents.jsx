8fab3eae816de76b887ad4a02f57742613705fc6
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2025-01-22 14:05:02 +0000

    test front 21

--- src/Authentification/InscriptionPrestataire/Component/FormComponents.jsx
+++ src/Authentification/InscriptionPrestataire/Component/FormComponents.jsx
@@ -181,16 +181,7 @@ const ServiceInfoForm = ({ formData, setFormData, serviceCategories }) => (
       />
     </FormField>
 
-    {/* <FormField label="Zone d'intervention" required>
-      <input 
-        type="text"
-        className={styles.input}
-        value={formData.serviceArea}
-        onChange={(e) => setFormData({...formData, serviceArea: e.target.value})}
-        required
-        placeholder="Ex: Rayon de 30km autour de Paris"
-      />
-    </FormField> */}
+   {/* test */}
   </div>
 );
 
@@ -221,8 +212,9 @@ const DocumentsForm = ({ formData, setFormData, handleFileUpload }) => (
         />
         <span className="text-sm text-gray-600">
           J'accepte les conditions générales du service
+        
         </span>
-      </label>
+      </label>/
     </div>
   </div>
 );
